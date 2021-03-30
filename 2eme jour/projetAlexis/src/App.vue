a7f97bb8d7b8ea9088818e837d533840e0858a0f
Nicolas Klarsfeld <nicolas.klarsfeld@gmail.com>
2021-03-30 09:14:07 +0200

    1st commit

---
+++ 2eme jour/projetAlexis/src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <input type="text" v-model="myTodo.title" />
+  <select v-model="priority">
+    <option v-for="option in priorities" :value="option.value">
+      {{ option.text }}
+    </option>
+  </select>
+  <button @click="addElement()">add</button>
+  <p>Priority low :</p>
+  <ul>
+    <Machin
+      v-for="element in lowListe"
+      v-bind:myElement="element"
+      v-on:deleteEvent="deleteElement($event)"
+    ></Machin>
+  </ul>
+
+  <p>Priority middle :</p>
+  <ul>
+    <Machin
+      v-for="element in middleListe"
+      v-bind:myElement="element"
+      v-on:deleteEvent="deleteElement($event)"
+    ></Machin>
+  </ul>
+
+  <p>Priority high :</p>
+  <ul>
+    <Machin
+      v-for="element in highListe"
+      v-bind:myElement="element"
+      v-on:deleteEvent="deleteElement($event)"
+    ></Machin>
+  </ul>
+  <br />
+  <br />
+
+  <MyComponent v-bind:variable="myTodo.title"></MyComponent>
+</template>
+
+<script>
+import Machin from "./components/Machin.vue";
+// import MyComponent from "./components/MyComponent.vue";
+
+export default {
+  name: "App",
+  components: { Machin }, // MyComponent
+  data() {
+    return {
+      myTodo: { title: "", priority: "high" },
+      liste: [
+        // middle high low
+        { id: 0, title: "1er todo", priority: "high" },
+        { id: 1, title: "2eme todo", priority: "middle" },
+        { id: 2, title: "3eme todo", priority: "low" },
+        { id: 3, title: "4eme todo", priority: "high" },
+        { id: 4, title: "5eme todo", priority: "low" },
+        { id: 5, title: "6eme todo", priority: "middle" },
+      ],
+      priority: "high",
+    };
+  },
+  methods: {
+    addElement() {
+      this.liste.push({
+        id: this.liste.length,
+        title: this.myTodo.title,
+        priority: "high",
+      });
+    },
+    deleteElement(id) {
+      this.liste.splice(
+        this.liste.findIndex((e) => e.id === id),
+        1
+      );
+    },
+  },
+  computed: {
+    lowListe() {
+      return this.liste.filter((e) => e.priority == "low");
+    },
+    middleListe() {
+      return this.liste.filter((e) => e.priority == "middle");
+    },
+    highListe() {
+      return this.liste.filter((e) => e.priority == "high");
+    },
+    priorities() {
+      console.log(Machin.data); 
+      return Machin.data;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ 2eme jour/projetAlexis/src/components/Machin.vue
@@ -0,0 +1,47 @@
+<template>
+  <li>
+    {{ myElement.title }} : {{ myElement.priority }}
+    <select v-model="myElement.priority">
+      <option v-for="option in options" :value="option.value">
+        {{ option.text }}
+      </option>
+    </select>
+
+    <!-- todo: choice of priorities, v-model sur myElement.priority -->
+    <button @click="deleteElement()">delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Machin",
+  props: {
+    myElement: {
+      type: Object,
+      default: { title: "titi", priority: "high" },
+    },
+  },
+  data() {
+    return {
+      options: [
+        { text: "High", value: "high" },
+        { text: "Middle", value: "middle" },
+        { text: "Low", value: "low" },
+      ],
+    };
+  },
+  mounted() {
+    this.showProps();
+  },
+  methods: {
+    deleteElement() {
+      this.$emit("deleteEvent", this.myElement.id);
+    },
+    showProps() {
+      console.log(this.myElement);
+    },
+  },
+};
+</script>
+
+<style></style>
